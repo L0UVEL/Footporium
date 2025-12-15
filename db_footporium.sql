54af243702407c8b9820fa1b07598881456eb218
L0UVEL <louvelrouz13@gmail.com>
2025-12-15 11:47:43 +0800

    UMAY

--- db_footporium.sql
+++ db_footporium.sql
@@ -3,7 +3,6 @@ USE db_footporium;
 
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
